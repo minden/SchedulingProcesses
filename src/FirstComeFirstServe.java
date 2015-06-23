599a46e82e86c9ec04596d17a5acf93cb9756494
Max Leonard Inden <inden@innoscale.de>
2015-06-24 00:07:22 +0200

    First sort implementation for FCFS

--- src/FirstComeFirstServe.java
+++ src/FirstComeFirstServe.java
@@ -1,11 +1,22 @@
-import java.lang.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by indenml on 21.06.15.
  */
 public class FirstComeFirstServe {
-    public static  Integer FirstComeFirstServe(ArrayList<java.lang.Process> processes){
+    public static  Integer FirstComeFirstServe(ArrayList<Process> processes){
+
+        Collections.sort(processes, new Comparator<Process>() {
+            @Override
+            public int compare(Process p1, Process p2) {
+                return p1.getArrivalTime().compareTo(p2.getArrivalTime());
+            }
+        });
+
+        Main.printProcesses(processes);
+
         return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
+import java.lang.*;
+import java.util.ArrayList;
+
 /**
  * Created by indenml on 21.06.15.
  */
 public class Main {
     public static void main(String[] args){
         CSVReader csvReader = new CSVReader();
-        processes = csvReader.read();
+        ArrayList<Process> processes = csvReader.read();
+        printProcesses(processes);
+        FirstComeFirstServe.FirstComeFirstServe(processes);
 
+    }
+
+    public static void printProcesses(ArrayList<Process> processes){
+        for(Process process: processes){
+            System.out.println("ProcessID: " + process.getProcessID());
+            System.out.println("ProcessingTime: " + process.getProcessingTime());
+            System.out.println("ArrivalTime: " + process.getArrivalTime());
+            System.out.println("IOBlockProbability: " + process.getIOBlockProbability());
+            System.out.println("IOBlockTime: " + process.getIOBlockTime());
+            System.out.println();
+        }
     }
 }
